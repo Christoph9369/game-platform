32d22a9abb1a6040478fb4c8d5ec544feb10472d
Christoph9369 <mojuyefelix@gmail.com>
2024-03-09 22:33:59 +0200

    generic Data fetching hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
-import { color } from "framer-motion";
-import React from "react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "cc0a6102a0a94c308cc883e063de2a38",
+    key: "74fbe2942191494fb3ac779907e237d5",
   },
 });
