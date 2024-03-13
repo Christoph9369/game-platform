a3aab720c1bde9881476ee78ed49cacee560bf44
Christoph9369 <mojuyefelix@gmail.com>
2024-03-13 12:59:19 +0200

    Built search Input

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -9,7 +9,7 @@ interface Props {
 }
 
 const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
-  const { data, error } = usePlatforms();
+  const { data } = usePlatforms();
 
   return (
     <Menu>
